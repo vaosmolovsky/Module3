214503785935f30f202d8e66c17ce51548dc8754
Name <email@example.ru>
2023-07-25 17:15:23 +0300

    MynameДобавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string myName = "Влад";
+            Console.WriteLine(myName);
+            Console.ReadKey();
+        }
+    }
+}
