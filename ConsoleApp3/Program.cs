462b3c69608b8568e3a69096757e2951abe1a7ac
Name <email@example.ru>
2023-07-27 17:47:26 +0300

    анкета

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -4,9 +4,18 @@
     {
         static void Main(string[] args)
         {
-            const string myName = "Влад";
-            Console.WriteLine(myName);
+            string myName = "Влад";
+            byte myAge = 29;
+            bool myPet = true;
+            double mySize = 44.5; 
+             
+            Console.WriteLine("Меня зовут " + myName);
+            Console.WriteLine("Мне {0} лет",myAge);
+            Console.WriteLine("Если ли у вас питомец? " +myPet);
+            Console.WriteLine("У меня {0} размер ноги",mySize);
             Console.ReadKey();
         }
     }
 }
+
+
